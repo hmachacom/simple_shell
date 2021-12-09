e72faea15b76904a27dd854a420156fad2587cf8
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-12-09 17:12:38 -0500

    env3

--- prompt.c
+++ prompt.c
@@ -12,7 +12,7 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 	const char *separador = " \t\n\"";
 	size_t u = 0, p = 1;
 	pid_t id = 0;
-	int status = 0, i = 0;
+	int status = 0;/*, i = 0;*/
 
 	signal(SIGINT, ctrolmasc);
 	while (1)
